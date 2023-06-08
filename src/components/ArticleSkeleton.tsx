0187a3f6b2cab42af3a163f624e7f9c4ee90c49e
Typhann <72917604+Typhann@users.noreply.github.com>
2023-06-08 17:11:32 +0200

    add skeleton darkmode

--- src/components/ArticleSkeleton.tsx
+++ src/components/ArticleSkeleton.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { useDarkMode } from "../utils";
 
 export default function ArticleSkeleton() {
   return (
     <div className="articles-container">
-      <div className="skeleton-article-container">
+      <div className={useDarkMode("skeleton-article-container")}>
         <div className="skeleton-img"></div>
         <div className="skeleton-headline"></div>
         <div className="skeleton-headline2"></div>
@@ -17,7 +18,7 @@ export default function ArticleSkeleton() {
         <div className="skeleton-text"></div>
         <div className="skeleton-link"></div>
       </div>
-      <div className="skeleton-article-container">
+      <div className={useDarkMode("skeleton-article-container")}>
         <div className="skeleton-img"></div>
         <div className="skeleton-headline"></div>
         <div className="skeleton-name-date-container">
