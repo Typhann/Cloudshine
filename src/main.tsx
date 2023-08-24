1728c8b458ce0ee6437e1d1d9e5de5ba9c3ec40e
Typhann <72917604+Typhann@users.noreply.github.com>
2023-08-24 10:21:54 +0200

    add todo comments

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { DarkModeProvider } from "./DarkModeContext.tsx";
 
+//TODO
+// cloudshine logo dissapears when click on searchbox on mobile
+// weird zoom in happens on mobile on safari when click on searchbox
+// all forms need a name or id attribute
+// horizontal scroll on mobile
+// infinite scroll on giannas phone but on mine the trending headlines shows up after
+// change infinite scroll position to load a bit before the end
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <DarkModeProvider>
